dcd8d6319c8bdd6821a819bb8a8d2bee29196860
bengo <171782+gobengo@users.noreply.github.com>
2024-07-27 07:04:52 -0700

    more working assertion-ndjson-to-tap

--- bin/assertion-ndjson-to-tap.js
+++ bin/assertion-ndjson-to-tap.js
@@ -8,17 +8,29 @@ import { ReadableStream } from "stream/web";
  * @param {object} options
  */
 async function main(options = {}) {
-  console.log('hi', process.stdin.isTTY, process.argv)
-  const assertionsFilePath = process.argv[2]
-  if (!existsSync(assertionsFilePath)) {
-    throw new Error(`file does not exist`, {
-      cause: assertionsFilePath
-    })
-  }
+  // whether the running process has a stdio pipe on stdin
+  const hasStdin = !process.stdin.isTTY
 
-  for await (const assertion of readNDJSONStream(ReadableStream.from(createReadStream(assertionsFilePath)))) {
+  // get a ReadableStream of ndjson assertions like those output by activitypub-testing
+  const assertionsStream = hasStdin ? ReadableStream.from(process.stdin) : createFileReadableStream(process.argv[2])
+
+  // log assertions
+  for await (const assertion of readNDJSONStream(assertionsStream)) {
     console.log('assertion', assertion)
   }
 }
 
 await main()
+
+/** given path to file, return a ReadableStream iff the file exists */
+function createFileReadableStream (assertionsFilePath) {
+  if (!assertionsFilePath) {
+    throw new Error(`provide an assertions file path as a positional argument`)
+  }
+  if (!existsSync(assertionsFilePath)) {
+    throw new Error(`file does not exist`, {
+      cause: assertionsFilePath
+    })
+  }
+  return ReadableStream.from(createReadStream(assertionsFilePath))
+}
