a38e65ca31c332ad3d60c5564b9268942c1eea80
bengo <171782+gobengo@users.noreply.github.com>
2024-08-05 09:56:04 -0700

    improve bin/assertion-ndjson-to-tap.js

--- bin/assertion-ndjson-to-tap.js
+++ bin/assertion-ndjson-to-tap.js
@@ -2,7 +2,9 @@
 
 import { createReadStream, existsSync } from "fs"
 import readNDJSONStream from 'ndjson-readablestream';
-import { ReadableStream } from "stream/web";
+import { Readable } from "stream";
+import { finished, pipeline } from "stream/promises"
+import { ReadableStream, WritableStream } from "stream/web";
 
 /**
  * @param {object} options
@@ -14,13 +16,49 @@ async function main(options = {}) {
   // get a ReadableStream of ndjson assertions like those output by activitypub-testing
   const assertionsStream = hasStdin ? ReadableStream.from(process.stdin) : createFileReadableStream(process.argv[2])
 
+  console.log('TAP version 14')
+
+  let assertionIndex = -1
+  process.stdout.setMaxListeners(1000) // to avoid nodejs warnings
   // log assertions
   for await (const assertion of readNDJSONStream(assertionsStream)) {
-    console.log('assertion', assertion)
+    assertionIndex++
+    const assertionsTap = Readable.from(renderAssertionTap(assertionIndex, assertion))
+    await pipeline(assertionsTap, process.stdout, { end: false })
+    process.stdout.write('\n\n')
   }
+  const plan = `1..${assertionIndex+1}\n`
+  process.stdout.write(plan)
 }
 
 await main()
+
+function * renderAssertionTap(index, assertion) {
+  const skipped = assertion.result.outcome === 'inapplicable'
+  const okness = assertion.result.outcome === 'passed' ? 'ok' : 'not ok'
+  yield `${okness} ${index} - ${assertion.test.name}`
+  if (skipped) yield ` # SKIP`
+  if (assertion.result.outcome === 'failed') {
+    // render the diagnostic block
+    function * renderDiagnostic (inner) {
+      yield * indented('  ', function * () {
+        yield '---\n'
+        yield 'foo: bar\n'
+        yield 'baz: bar\n'
+        yield '...\n'
+      }())
+    }
+    yield '\n'
+    yield * renderDiagnostic('foo')
+  }
+}
+
+function * indented(prefix, content) {
+  for (const c of content) {
+    yield prefix
+    yield c
+  }
+}
 
 /** given path to file, return a ReadableStream iff the file exists */
 function createFileReadableStream (assertionsFilePath) {
